3eef9aced281c1dba2b17ba6d85b857edd0fea3d
MantasValentin <mantasvalentin@protonmail.com>
2022-12-13 21:08:20 +0200

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#cdcdcd',
+        secondary: '#e6e6e6',
+        third: '#b0b0b0',
+        fourth: '#909090'
+      },
+    },
+  },
+  plugins: [],
+}
